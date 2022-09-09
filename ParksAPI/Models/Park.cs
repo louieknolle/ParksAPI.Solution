1ff0e679a683ead324648fa617a827fd6447dc55
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-09-08 21:01:44 -0400

    park model and parks context model

---
+++ ParksAPI/Models/Park.cs
@@ -0,0 +1,9 @@
+namespace ParksAPI.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string StateLocation { get; set; }
+    }
+}
